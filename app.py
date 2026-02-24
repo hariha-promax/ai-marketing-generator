a4283be75b23f2b48f4ed7edc15fb202034b13a2
hariha-promax <harirajasekaran2211@gmail.com>
2026-02-24 11:25:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import gradio as gr
-from transformers import pipeline
+import requests
+import os
 
-generator = pipeline("text-generation", model="google/flan-t5-small")
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-small"
+API_KEY = os.environ.get("HF_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
 
 def generate(product, audience, tone, content_type):
+
     prompt = f"""
     Generate a {content_type}.
     Product: {product}
@@ -11,8 +18,19 @@ def generate(product, audience, tone, content_type):
     Tone: {tone}.
     Make it persuasive and creative.
     """
-    result = generator(prompt, max_length=150)
-    return result[0]["generated_text"]
+
+    response = requests.post(
+        API_URL,
+        headers=headers,
+        json={"inputs": prompt}
+    )
+
+    result = response.json()
+
+    if isinstance(result, list):
+        return result[0]["generated_text"]
+    else:
+        return "Model is loading. Try again in 20 seconds."
 
 demo = gr.Interface(
     fn=generate,
@@ -26,6 +44,5 @@ demo = gr.Interface(
     title="AI Marketing Content Generator"
 )
 
-import os
 port = int(os.environ.get("PORT", 10000))
 demo.launch(server_name="0.0.0.0", server_port=port)
